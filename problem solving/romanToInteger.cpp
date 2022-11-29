ea5229c78a45b4d7f281659091d777f2b4d75882
Imran Sarkar Sabbir <imran.sarkar@softbdltd.com>
2022-11-29 09:36:03 +0600

    roman number functiooooon update.

--- problem solving/romanToInteger.cpp
+++ problem solving/romanToInteger.cpp
@@ -17,6 +17,26 @@ std::unordered_map<std::string, int> romanValues = {
     {"M", 1000},
 };
 
+int getValue(std::string token) {
+    
+    if(token == "I") return 1;
+    else if(token == "IV") return 4;
+    else if(token == "V") return 5;
+    else if(token == "IX") return 9;
+    else if(token == "X") return 10;
+    else if(token == "XL") return 40;
+    else if(token == "L") return 50;
+    else if(token == "XC") return 90;
+    else if(token == "C") return 100;
+    else if(token == "CD") return 400;
+    else if(token == "D") return 500;
+    else if(token == "CM") return 900;
+    else if(token == "M") return 1000;
+
+    return 0;
+}
+
+
 int romanToInt(std::string s)
 {
 
@@ -28,17 +48,17 @@ int romanToInt(std::string s)
 
     for (; index < end; index++)
     {
-        auto value = romanValues.find(s.substr(index, 2));
+        auto value = getValue(s.substr(index, 2));
 
-        if (value != map_end)
+        if (value != 0)
         {
-            result += value->second;
+            result += value;
             index++;
         }
         else
         {
-            auto value = romanValues.find(s.substr(index, 1));
-            result += value->second;
+            auto value = getValue(s.substr(index, 1));
+            result += value;
         }
     }
 
